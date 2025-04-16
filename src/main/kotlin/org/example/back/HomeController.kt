b52744bb4b1d463b6561219c1700d03ceaa1f33c
dlfjsld1 <ltk900216@gmail.com>
2025-04-16 14:17:31 +0900

    배포자동화를 위해 github action 파일 추가

--- src/main/kotlin/org/example/back/HomeController.kt
+++ src/main/kotlin/org/example/back/HomeController.kt
@@ -1,5 +1,6 @@
 package org.example.back
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import software.amazon.awssdk.services.s3.S3Client
@@ -8,9 +9,13 @@ import software.amazon.awssdk.services.s3.S3Client
 class HomeController(
     private val s3Serivce: S3Service
 ) {
+
+    @Value("\${custom.secretWord}")
+    private val secretWord: String = ""
+
     @GetMapping
     fun main(): String {
-        return "Hi"
+        return "Hi, $secretWord"
     }
 
     @GetMapping("/buckets")
